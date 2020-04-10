7d8f34eac87e5260d5eeead857fc6e48a7d94311
narendrajava1 <narendrakolli08@gmail.com>
2020-04-10 13:34:50 +0530

    added data.sql and h2 db properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO NOTES VALUES (1,'first day','2020-04-01 13:13:51.807','this is 1st day','2020-04-02 13:13:51.807');
+INSERT INTO NOTES VALUES (2,'second day','2020-04-02 13:13:51.807','this is 2nd day','2020-04-03 13:13:51.807');
+INSERT INTO NOTES VALUES (3,'thrid day','2020-04-03 13:13:51.807','this is 3rd day','2020-04-04 13:13:51.807');
+INSERT INTO NOTES VALUES (4,'fourth day','2020-04-04 13:13:51.807','this is 4th day','2020-04-05 13:13:51.807');
+INSERT INTO NOTES VALUES (5,'fiveth day','2020-04-05 13:13:51.807','this is 5th day','2020-04-06 13:13:51.807');
+INSERT INTO NOTES VALUES (6,'six day','2020-04-06 13:13:51.807','this is 6th day','2020-04-07 13:13:51.807');
+INSERT INTO NOTES VALUES (7,'seventh day','2020-04-07 13:13:51.807','this is 7th day','2020-04-08 13:13:51.807');
+INSERT INTO NOTES VALUES (8,'eight day','2020-04-08 13:13:51.807','this is 8th day','2020-04-09 13:13:51.807');
